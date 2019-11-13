84d42164699d23f8bb8c93fb8e401c5195b6113f
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2019-11-13 17:07:06 -0600

    ReadingVideo using frame based img start point instead of time based.

--- fixed-gaze/preprocessing/ReadingVideo.py
+++ fixed-gaze/preprocessing/ReadingVideo.py
@@ -1,89 +1,109 @@
 """
 Created on Thu Nov  8 19:14:19 2018
-
-@author: mfm160330, aps170830
+@author: mfm160330
 """
+
 import numpy as np
 import cv2
 import os
 import argparse
 
 
+'''
+    Parsing 3 arguments from the command line
+        >   video_path  str     path to the video file to split into images
+        >   write_path  str     path of the output folder to spill images into
+        >   frame_start int     starting frame in the video to begin outputting images
+'''
+
+
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('video', help='Path to video', type=str)
-    parser.add_argument(
-        'output', help='Directory to output images out to', type=str)
-    parser.add_argument(
-        'startMinute', help='Start minute to clip from', type=int)
-    parser.add_argument(
-        'startSecond', help='Start second to clip from', type=int)
-    parser.add_argument('endMinute', help='End minute to clip to', type=int)
-    parser.add_argument('endSecond', help='End second to clip to', type=int)
+    args = argparse.ArgumentParser()
 
-    return parser.parse_args()
+    args.add_argument(
+        'video_path', help='Video to split into images', type=str)
+    args.add_argument(
+        'write_path', help='EXISTING folder to write the output images to', type=str)
 
+    args.add_argument(
+        'image_prefix', help='Image prefix for output images', type=str
+    )
+    args.add_argument(
+        'frame_start', help='Start frame to begin outputting images from', type=int, default=0)
 
-##===== Intialize the start and end minute/second to be extracted from the video ===#
-FirstFrameFlag = 1
-MinuteStart = 0
-SecStart = 0  # 41
-
-MinuteEnd = 4
-SecEnd = 55  # 24
-
-#ReadVideoLocation = 'F:/TestDrive2Oct18/Face Camera'
-# 'G:/Multi-sensors gaze Data Collection/TestDrive2018-12-03/Face/F4.MP4'
-VideoName = 'G:/Multi-sensors gaze Data Collection/Drive 2019-7-23/Face/GH030193.MP4'
-WriteLocation = 'G:/FixedGazeImages/Gaze points Data/G2019-7-23'
-ImagesName = 'D2019-7-23'
-FR = 60
-
-# 180 #(face14Oct18: 50, face1Dec: 100   mirror:150 ) #Vertical Crop Start
-vCropS = 80
-vCropE = 1000  # (face14Oct18: 1000,  mirror:950) #Vertical crop end
-hCropS = 500  # (face14Oct18: 500,  mirror:800)
-hCropE = 1550  # (face14Oct18: 1600, mirror:1650)
-
-if __name__ == "__main__":
-    args = parse_args()
-    output_images(args.video, args.output, args.startSecond,
-                  args.startMinute, args.endMinute, args.endSecond, FR)
-    exit()
-
-##========Read Video =======##
+    return args.parse_args()
 
 
-def output_images(video_name, write_location, minute_start, second_start, minute_end, second_end, frame_rate):
-    cap = cv2.VideoCapture(video_name)
-    msStart = (minute_start * 60 + second_start) * 1000
-    cap.set(cv2.CAP_PROP_POS_MSEC, msStart)
+def GetFrameIds(frame):
+    return (np.floor(frame/3600), np.floor((frame % 3600) / 60), frame % 60)
 
-    frame_end = (minute_end * 60 + second_end) * frame_rate
+
+def ReadingVideo(video_name, write_path, image_prefix, frame_start):
+    VideoName = video_name
+    WriteLocation = write_path
+    ImagesName = image_prefix
+
+    #==== Intializing the crop values in HD===================#
+    cropFlag = 1
+
+    # 180 #(face14Oct18: 50, face1Dec: 100   mirror:150 ) #Vertical Crop Start
+
+    vCropS = 80
+    vCropE = 1000  # (face14Oct18: 1000,  mirror:950) #Vertical crop end
+    hCropS = 500  # (face14Oct18: 500,  mirror:800)
+    hCropE = 1550  # (face14Oct18: 1600, mirror:1650)
+
+    ##========Read Video =======##
+    cap = cv2.VideoCapture(VideoName)
+    cap.set(cv2.CAP_PROP_POS_MSEC, 1)
+
+    if not cap.isOpened():
+        print("Unable to open video %s" % (video_name))
+        exit()
+
+    print(write_path)
+
+    video_size = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 
     count = 0
-    should_read_more_frames = True
-    while(cap.isOpened() & should_read_more_frames):
-        _, frame = cap.read()
+    frame_well_read = True
 
+    while(cap.isOpened() & frame_well_read & count <= video_size):
+        frame_well_read, frame = cap.read()
         if frame is None:
-            count += 1
+            print("%d is a null frame" % (count))
             continue
 
         frameCropped = frame[vCropS:vCropE, hCropS:hCropE]
 
-        currentFrameNumber = cap.get(
-            cv2.CAP_PROP_POS_FRAMES)  # current frame number
-        second_name = np.mod(second_start+np.floor(count/60), 60)
-        minute_name = minute_start + np.floor(second_start/60 + count/3600)
-        # SecFracName is 1/60 (1 frame) seconds
-        secondFractionName = np.mod(count, 60)
+        if count < frame_start:
+            count = count + 1
+            continue
 
-        cv2.imwrite(WriteLocation+"/"+ImagesName+"-%d-%d-%d-%d.jpg" %
-                    (count, minute_name, second_name, secondFractionName), frameCropped)
-        should_read_more_frames = bool(frame_end - currentFrameNumber)
+        # cv2.imshow('frame',frame) # PlZ comment me before running (To display the image)
+        # cv2.imshow('frameCropped',frameCropped) # PlZ comment me before running (To display the image)
+        # cv2.waitKey(0) # PlZ comment me before running
+        framesSinceStart = count - frame_start
+
+        (minName, secName, secFrameName) = GetFrameIds(framesSinceStart)
+
+        name = WriteLocation+"/"+ImagesName + \
+            "-%d-%d-%d-%d.jpg" % (framesSinceStart,
+                                  minName, secName, secFrameName)
+
+        print(name)
+
+        cv2.imwrite(name, frameCropped)
         count += 1
 
     cap.release()
     cv2.destroyAllWindows()
+
     print("Finished without Errors")
+
+
+if __name__ == "__main__":
+
+    prg_args = parse_args()
+    ReadingVideo(prg_args.video_path, prg_args.write_path,
+                 prg_args.image_prefix, prg_args.frame_start)
